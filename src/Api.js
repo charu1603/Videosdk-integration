8194bafbfc090b0ad159d035e4d6f1295b9aa532
charu1603 <kakdechaitrali16@gmail.com>
2025-09-06 11:39:25 +0530

    remove redundant code

--- src/Api.js
+++ src/Api.js
@@ -1,33 +1,23 @@
-
 export const authToken = import.meta.env.VITE_AUTH_TOKEN;
 
-// VideoSDK API configuration
-// Replace with your actual token
+const createRoom = async (token) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+
+  const { roomId } = await res.json();
+  return roomId;
+};
 
 export const createMeeting = async ({ token }) => {
-  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
-    method: "POST",
-    headers: {
-      authorization: `${token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  })
-
-  const { roomId } = await res.json()
-  return roomId
-}
+  return await createRoom(token);
+};
 
 export const createStream = async ({ token }) => {
-  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
-    method: "POST",
-    headers: {
-      authorization: `${token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  })
-
-  const { roomId } = await res.json()
-  return roomId
-}
+  return await createRoom(token);
+};
